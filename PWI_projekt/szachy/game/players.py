5df72337f9225b62c98d8ff46ed2a1dee0791398
julia <julie.szczepek@gmail.com>
2024-01-14 01:11:07 +0100

    Start

---
+++ PWI_projekt/szachy/game/players.py
@@ -0,0 +1,83 @@
+import pygame as pg
+from game.board import *
+from game.game import *
+from game.options import Menu
+from game.DumbBot import *
+
+pg.init()
+
+def main():
+    menu = Menu()
+    selected_option = menu.run()
+
+    if (
+        selected_option == "Player vs Player"
+        or selected_option == "Player vs Bot - White - Dumb Bot"
+        or selected_option == "Player vs Bot - White - Smart Bot"
+        or selected_option == "Player vs Bot - Black - Dumb Bot"
+        or selected_option == "Player vs Bot - White - Dumb Bot"
+    ):
+        play_player_vs_player(selected_option)
+
+    elif selected_option == "Bot vs Bot":
+        play_bot_vs_bot()
+
+
+def play_player_vs_player(selected_option):
+    screen_size = 800
+    screen = pg.display.set_mode((screen_size, screen_size))
+    pg.display.set_caption("Szachy")
+    white = (220, 230, 255)
+    brown = (205, 133, 63)
+    board = ChessBoard(screen_size, (white, brown), screen)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                break
+
+        screen.fill((0, 0, 0))
+        board.draw_board()
+        highlight(screen)
+        board.move_piece()
+        board.draw_pieces()
+
+        pg.display.flip()
+
+
+def play_bot_vs_bot():
+    screen_size = 800
+    screen = pg.display.set_mode((screen_size, screen_size))
+    pg.display.set_caption("Szachy")
+    white = (220, 230, 255)
+    brown = (205, 133, 63)
+    board = ChessBoard(screen_size, (white, brown), screen)
+
+    bot1 = DumbBot("w")
+    bot2 = DumbBot("b")
+
+    game = Game(board, bot1, bot2)
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                break
+
+        screen.fill((0, 0, 0))
+        board.draw_board()
+        board.draw_pieces()
+
+        if game.current_turn == "w":
+            time.sleep(2)  # Wait 2 seconds before making a move for better visibility
+            bot1.make_move(board)
+        else:
+            time.sleep(2)  # Wait 2 seconds before making a move for better visibility
+            bot2.make_move(board)
+
+        pg.display.flip()
+
+
+if __name__ == "__main__":
+    main()
