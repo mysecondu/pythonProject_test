8675b8b08515702f8d45b839cfd28bb7aada41c0
julia <julie.szczepek@gmail.com>
2024-01-17 16:07:24 +0100

    kom

--- PWI_projekt/szachy/game/dumb_bot.py
+++ PWI_projekt/szachy/game/dumb_bot.py
@@ -22,7 +22,6 @@ class DumbBot:
     def get_random_move(self, board):
         valid_moves = self.get_all_valid_moves(board)
         if not valid_moves:
-            print("NNNNNNNNNNNNNNNIE")
             return None
 
         return random.choice(valid_moves)
@@ -36,20 +35,20 @@ class DumbBot:
                 #print("piece", piece)
                 if piece != 0 and piece.color == self.color:
                     if isinstance(piece, King):
-                        possible_moves = piece.possible_moves
-
-                    elif isinstance(piece, Pawn):
-                        possible_moves = piece.possible_moves
-                        print("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPawn")
+                        possible_moves_db = piece.possible_moves_f(board, self.w_attacked, self.b_attacked)
+                        cośtam = piece.possible_moves
 
                     else:
-                        possible_moves = piece.possible_moves
+                        possible_moves_db = piece.possible_moves_f(board)
+                        cośtam = piece.possible_moves
 
-                    print("possible moves",possible_moves)
+
+                    #print("possible moves",possible_moves_db)
+                    print("cośtam", cośtam)
 
                     # Check if possible_moves is None
-                    if possible_moves is not None:
-                        for move in possible_moves:
+                    if cośtam is not None and cośtam is not []:
+                        for move in cośtam:
                             valid_moves.append(((i, j), move))
         print("valid moves:", valid_moves)
         return valid_moves
